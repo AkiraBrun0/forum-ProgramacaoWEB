9fdeb71d29a7a7ce88cb8f37ccdc986ff12ff829
Bruno de Menezes <brunosjc.92@hotmail.com>
2022-10-12 17:30:49 -0300

    ajustes

--- mostrar-signo.php
+++ mostrar-signo.php
@@ -9,19 +9,16 @@
 </head>
 
 <body>
-  <div>
-    <div>
-      <h2>Seu signo é 
-        <?php echo $signoUsuario['nome'] ?> 
-      </h2>
-      <p>Atributos do signo: <?php echo $signoUsuario['dataInicio'] ?> até <?= $signoUsuario['dataFim'] ?>
-        <?php 
-        echo "<pre>";
-        echo $signoUsuario['descricao'];
-        echo "</pre>"; ?> 
-      </p>
-    </div>
-  </div>
+  <h2>Seu signo é
+    <?php echo $signoUsuario['nome'] ?>
+  </h2>
+  <h3>
+  <?php echo $signoUsuario['dataInicio'] ?> até <?= $signoUsuario['dataFim'] ?>
+  </h3>
+  <h4>Descrição do signo: </h4>
+  <p>
+    <?php echo $signoUsuario['descricao']; ?>
+  </p>
   <input type="button" value="Voltar" onClick="history.go(-1)">
 </body>
 
